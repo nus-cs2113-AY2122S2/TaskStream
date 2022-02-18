af56694393babed95e3746d9d8852bc9e1b371ff
matheril <sauravvm237@gmail.com>
2022-02-18 17:52:34 +0800

    master

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -11,7 +11,7 @@ import java.util.Locale;
 import static java.util.stream.Collectors.toList;
 
 public class Main {
-    //streams
+    //master
 
     private TaskNameComparator taskNameComparator;
 
